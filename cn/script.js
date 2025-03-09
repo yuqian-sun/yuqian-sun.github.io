4f6e481d51d9e8b9ca36bf125dc2056736c97071
yuqian-sun <sienna.sun.game@gmail.com>
2025-03-09 18:46:40 -0400

    update cn file

---
+++ cn/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new fullpage("#fullpage", {
+        autoScrolling: true,
+        navigation: true,
+        navigationPosition: "right",
+        scrollingSpeed: 700,
+    });
+});
+
