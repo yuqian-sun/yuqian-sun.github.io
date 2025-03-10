7f3356183cde6df28e2736a91955f33e3bcaefcc
yuqian-sun <sienna.sun.game@gmail.com>
2025-03-09 22:29:47 -0400

    page framework ready

--- cn/script.js
+++ cn/script.js
@@ -4,6 +4,28 @@ document.addEventListener("DOMContentLoaded", function () {
         navigation: true,
         navigationPosition: "right",
         scrollingSpeed: 700,
+
+        anchors: ['about', 'works', 'personalWorks', 'contact'],
+        menu: 'header nav ul',
+        normalScrollElements: '.hero-details',
+        scrollOverflow: true,
+        
+        // 控制滚动敏感度，防止滚动过快
+        touchSensitivity: 10,
+        
+        // 这个选项可以防止快速滚动跳过页面
+        continuousVertical: false
+    });
+
+    // 获取英雄区域容器
+    const heroWrapper = document.querySelector('.hero-wrapper');
+    heroWrapper.addEventListener('mouseenter', function () {
+        heroWrapper.classList.add('hovered');
+    });
+    heroWrapper.addEventListener('mouseleave', function () {
+        heroWrapper.classList.remove('hovered');
     });
 });
+
+
 
